28323e5015f9a19c0ba3f8be4bc16dde32c94321
emoraisdev <evertonsmorais@gmail.com>
2024-07-27 15:03:22 -0300

    Criando Projeto Gestão Pagamentos

---
+++ mspagamentos/src/main/java/br/com/fiap/mspagamentos/MspagamentosApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.mspagamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MspagamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MspagamentosApplication.class, args);
+	}
+
+}
